fb333713fcc2d5e281f912ca951c500b7a3f8037
SO YEON LEE <lana_75@naver.comgit>
2024-05-09 16:26:31 +0900

    기본 틀 세팅

---
+++ src/main/java/com/example/Fruitemarketbuyer2Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fruitemarketbuyer2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fruitemarketbuyer2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fruitemarketbuyer2/Fruitemarketbuyer2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fruitemarketbuyer2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fruitemarketbuyer2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
